9f50207ad551f961c263372af9205cd2e685f10a
choisangwon <choisangwon2170@daum.net>
2020-07-12 20:43:14 +0900

    db update

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const express = require('express');
 const app = express();
 const moran = require('morgan');
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
 const productRoute = require('./routes/product');
 const orderRoute = require('./routes/order');
+const { Mongoose } = require('mongoose');
 
 // // 리퀘스트 리스폰스 테스트
 // app.use((req, res) => {
@@ -19,6 +21,24 @@ const orderRoute = require('./routes/order');
 // // 제이슨은 현재 가장많이 사용하고 있는 데이터 구조
 // //인터넷에서 검색해 볼 것 (제이슨)
 //주석처리 단축키 : 커맨드+슬레시
+
+
+//데이터베이스 연결
+const dbAdress = "mongodb+srv://admin:1350110@cluster0.hit8h.mongodb.net/node_shoppingmall?retryWrites=true&w=majority";
+
+const DB_Options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose
+    .connect(dbAdress, DB_Options)
+    .then(() => console.log("MongoDB connected ..."))
+    .catch(err => console.log(err.message));
+
+
+
+
 
 //미드웨어 설정
 app.use(moran('dev'));
